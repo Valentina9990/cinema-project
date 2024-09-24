eeb2b626a232b4a828b4105b53a0128beb90883f
DavuKX <duvandavid2001@gmail.com>
2024-09-23 21:40:38 -0500

    Eliminar carpeta build del repositorio

--- build/config/connection/optionsPG.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.optionsPG = void 0;
-const camelCase_1 = require("./camelCase");
-exports.optionsPG = {
-    receive(e) {
-        (0, camelCase_1.camelizeColums)(e.data);
-    },
-};
